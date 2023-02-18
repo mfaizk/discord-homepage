b23c16bcc6b4f70c5c2b8f766997329870584f77
mfaizk <faizk123485@gmail.com>
2023-02-18 18:42:49 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+      },
+      backgroundImage: {
+        "hero-bg": "url('./assets/hero-bg.svg')",
+      },
+    },
+  },
+};
